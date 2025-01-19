c9f4f4363da846e3bab74db801853663ed3604fb
Josiassantos1577 <josiassantos1577@gmail.com>
2025-01-18 22:42:16 -0300

    feat: Initial commit

---
+++ src/main/java/com/josias/prescricao_api/PrescricaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.josias.prescricao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescricaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrescricaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josias/prescricao_api/PrescricaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josias.prescricao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescricaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
